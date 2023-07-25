6d27a73b4fec77fdf2603ddd955c5debe8b65092
kitro <kitrofimov39@gmail.com>
2023-07-25 14:29:43 +0500

    pretty-formatted Camera.hpp

--- src/Camera/Camera.hpp
+++ src/Camera/Camera.hpp
@@ -8,17 +8,23 @@ public:
     enum Direction {forward, backward, left, right};
 
     Camera(glm::vec3 position, float fov, float speed, Window* window);
+
     void update();
     void move(Direction direction, double delta_time);
+
     glm::mat4 get_view_matrix();
     glm::mat4 get_projection_matrix();
+
 private:
     glm::vec3 position;
     glm::vec3 front;
     static constexpr glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+
     glm::mat4 view;
     glm::mat4 projection;
+
     float fov;
     float speed;
+
     Window* window;
 };
